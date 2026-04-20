36f3b37e5895af4bda38641d84ae8650890dfa21
Mark Otto <markdotto@gmail.com>
2026-04-20 14:07:33 -0700

    More launch prep (#560)
    * better colors, simplier logic
    
    * some minor copy
    
    * Simpler and smarter reset for context menu
    
    * dgit status table
    
    * simpler
    
    * better
    
    * fix context menu height and padding
    
    * tweaks
    
    * sticky
    
    * images ad icons

--- apps/docs/app/trees/dimensions.ts
+++ apps/docs/app/trees/dimensions.ts
@@ -1,9 +1,9 @@
 export const TREE_NEW_DEFAULT_VIEWPORT_HEIGHT = 716;
 
 export const TREE_NEW_VIEWPORT_HEIGHTS = {
-  a11y: TREE_NEW_DEFAULT_VIEWPORT_HEIGHT,
-  contextMenu: 770,
-  customIcons: TREE_NEW_DEFAULT_VIEWPORT_HEIGHT + 42, // for search bar
+  a11y: 760,
+  contextMenu: 760,
+  customIcons: TREE_NEW_DEFAULT_VIEWPORT_HEIGHT, // for search bar
   densityCompact: 362,
   densityDefault: 446,
   densityRelaxed: 530,
@@ -13,9 +13,9 @@ export const TREE_NEW_VIEWPORT_HEIGHTS = {
   gitStatusFiltered: 270,
   gitStatusFull: 720,
   middotTruncation: 690,
-  searchCollapseNonMatches: 760,
-  searchExpandMatches: 968,
-  searchHideNonMatches: 398,
+  searchHideNonMatches: 310,
+  searchCollapseNonMatches: 820,
+  searchExpandMatches: 1030,
   styling: 438,
   theming: TREE_NEW_DEFAULT_VIEWPORT_HEIGHT,
   treeApp: 720,
